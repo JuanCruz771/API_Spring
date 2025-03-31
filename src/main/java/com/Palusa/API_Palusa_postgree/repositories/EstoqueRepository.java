0a0eccbc244376fdcac092c1e654c7b39a1a1dd1
JuanCruz771 <gabrielcruz.juan.771@gmail.com>
2025-03-30 21:10:59 -0300

    @

--- src/main/java/com/Palusa/API_Palusa_postgree/repositories/EstoqueRepository.java
+++ src/main/java/com/Palusa/API_Palusa_postgree/repositories/EstoqueRepository.java
@@ -4,6 +4,6 @@ import com.Palusa.API_Palusa_postgree.models.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@@Repository
+@Repository
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
 }
